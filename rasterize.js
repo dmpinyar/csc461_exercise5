9e3186dba575e4f47da0a08f4c32ba17294c07a9
dmpinyar <dmpinyard@gmail.com>
2026-09-25 16:47:15 -0400

    Change fragment color in shader from white to purple

--- rasterize.js
+++ rasterize.js
@@ -170,7 +170,7 @@ function setupShaders() {
     // define fragment shader in essl using es6 template strings
     var fShaderCode = `
         void main(void) {
-            gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0); // all fragments are white
+            gl_FragColor = vec4(0.7, 0.05, 0.7, 1.0);
         }
     `;
     
